b1081706f4cbf9e464f40a038a089f71dbf7876a
“Thushara1020” <thusharabawantha2001@gmail.com>
2026-02-07 15:24:44 +0530

    integrate DashboardComponent as the primary home view

--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from "../../component/navbar/navbar.component";
 import { FoodcardComponent } from "../../component/foodcard/foodcard.component";
 import { OrderSummaryComponent } from "../../component/order-summary/order-summary.component";
+import { DashboardComponent } from "../../component/dashboard/dashboard.component";
 
 @Component({
   selector: 'app-home',
-  imports: [NavbarComponent, FoodcardComponent, OrderSummaryComponent],
+  imports: [NavbarComponent,DashboardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
