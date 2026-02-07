d0744580a3c28561f8694187f447ec22e26deed2
“Thushara1020” <thusharabawantha2001@gmail.com>
2026-02-07 12:04:20 +0530

    integrate OrderSummaryComponent and connect CartService logic

--- src/app/component/foodcard/foodcard.component.ts
+++ src/app/component/foodcard/foodcard.component.ts
@@ -1,24 +1,28 @@
 import { Component, inject } from '@angular/core';
 import { Product, products } from "../../../model/products";
-import { CartService } from "../../../cart.service"; 
+import { CartService } from "../../../cart.service";
+import { OrderSummaryComponent } from "../order-summary/order-summary.component"; 
 
 @Component({
   selector: 'app-foodcard',
   standalone: true, 
-  imports: [],
+  imports: [OrderSummaryComponent],
   templateUrl: './foodcard.component.html',
   styleUrl: './foodcard.component.css'
 })
 export class FoodcardComponent {
   products: Product[] = products;
+  selectedProductIds: (number | string)[] = [];
 
   private cartService = inject(CartService);
 
   constructor() {}
 
-
-  addToOrder(item: Product) {
+    addToOrder(item: Product) {
     this.cartService.addItem(item);
-    console.log(item.id);
+    
+    this.selectedProductIds.push(item.id,item.name,item.price,item.image);
+    
+    console.log('Current ID Array:', this.selectedProductIds);
   }
-}
+  }
