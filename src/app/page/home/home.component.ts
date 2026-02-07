0861a7533ff24d09798fedbbc4f7d14e31752e1d
“Thushara1020” <thusharabawantha2001@gmail.com>
2026-02-07 12:07:08 +0530

    register OrderSummaryComponent in HomeComponent imports

--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "../../component/navbar/navbar.component";
 import { FoodcardComponent } from "../../component/foodcard/foodcard.component";
+import { OrderSummaryComponent } from "../../component/order-summary/order-summary.component";
 
 @Component({
   selector: 'app-home',
-  imports: [NavbarComponent, FoodcardComponent],
+  imports: [NavbarComponent, FoodcardComponent, OrderSummaryComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
