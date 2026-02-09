a27376576eab2b7d9f9df884131f47893464c10f
“Thushara1020” <thusharabawantha2001@gmail.com>
2026-02-09 20:16:17 +0530

    @ -1,7 +1,7 @@ import { Component, inject } from '@angular/core'; import { Product, products } from "../../../model/products"; import { Component, inject, OnInit } from '@angular/core'; // 1. OnInit import කරන්න import { Product } from "../../../model/products"; import { CartService } from "../../../cart.service"; import { OrderSummaryComponent } from "../order-summary/order-summary.component";  import { OrderSummaryComponent } from "../order-summary/order-summary.component";  @Component({   selector: 'app-foodcard', @ -10,19 +10,36 @@ import { OrderSummaryComponent } from "../order-summary/order-summary.component"   templateUrl: './foodcard.component.html',   styleUrl: './foodcard.component.css' }) export class FoodcardComponent {   products: Product[] = products;   selectedProductIds: (number)[] = []; export class FoodcardComponent implements OnInit {      products: Product[] = [];    selectedProductIds: number[] = [];    private cartService = inject(CartService);    constructor() {}      addToOrder(item: Product) {   ngOnInit() {   this.cartService.getAllItemsFromApi().subscribe(data => {     console.log("Backend එකෙන් ආපු බඩු:", data);     this.products = data;   }); }    loadProducts() {     this.cartService.getAllItemsFromApi().subscribe({       next: (data: Product[]) => {         this.products = data;          console.log('Data loaded successfully:', data);       },       error: (err) => {         console.error('API Error:', err);         alert('Backend is Nor Work!');       }     });   }    addToOrder(item: Product) {     this.cartService.addItem(item);          this.selectedProductIds.push(item.id);          console.log('Current ID Array:',this.selectedProductIds);   }   } }

--- src/app/component/foodcard/foodcard.component.ts
+++ src/app/component/foodcard/foodcard.component.ts
@@ -1,7 +1,7 @@
-import { Component, inject } from '@angular/core';
-import { Product, products } from "../../../model/products";
+import { Component, inject, OnInit } from '@angular/core'; // 1. OnInit import කරන්න
+import { Product } from "../../../model/products";
 import { CartService } from "../../../cart.service";
-import { OrderSummaryComponent } from "../order-summary/order-summary.component"; 
+import { OrderSummaryComponent } from "../order-summary/order-summary.component";
 
 @Component({
   selector: 'app-foodcard',
@@ -10,19 +10,36 @@ import { OrderSummaryComponent } from "../order-summary/order-summary.component"
   templateUrl: './foodcard.component.html',
   styleUrl: './foodcard.component.css'
 })
-export class FoodcardComponent {
-  products: Product[] = products;
-  selectedProductIds: (number)[] = [];
+export class FoodcardComponent implements OnInit {
+  
+  products: Product[] = []; 
+  selectedProductIds: number[] = [];
 
   private cartService = inject(CartService);
 
-  constructor() {}
 
-    addToOrder(item: Product) {
+  ngOnInit() {
+  this.cartService.getAllItemsFromApi().subscribe(data => {
+    console.log("Backend එකෙන් ආපු බඩු:", data);
+    this.products = data;
+  });
+}
+
+  loadProducts() {
+    this.cartService.getAllItemsFromApi().subscribe({
+      next: (data: Product[]) => {
+        this.products = data; 
+        console.log('Data loaded successfully:', data);
+      },
+      error: (err) => {
+        console.error('API Error:', err);
+        alert('Backend is Nor Work!');
+      }
+    });
+  }
+
+  addToOrder(item: Product) {
     this.cartService.addItem(item);
-    
     this.selectedProductIds.push(item.id);
-    
-    console.log('Current ID Array:',this.selectedProductIds);
   }
-  }
+}
