2f9ece6bbeb5ca8966319f851945ac86dd61b37b
“Thushara1020” <thusharabawantha2001@gmail.com>
2026-02-10 13:02:44 +0530

    register sub-components in InventoryComponent imports for dashboard layout

---
+++ src/app/page/inventory/inventory.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from "../../component/navbar/navbar.component";
+import { FoodcardComponent } from "../../component/foodcard/foodcard.component";
+import { AdditemsComponent } from "../../component/additems/additems.component";
+import { RemovitemsComponent } from "../../component/removitems/removitems.component";
+
+@Component({
+  selector: 'app-inventory',
+  imports: [NavbarComponent, FoodcardComponent, AdditemsComponent, RemovitemsComponent],
+  templateUrl: './inventory.component.html',
+  styleUrl: './inventory.component.css'
+})
+export class InventoryComponent {
+
+}
