fa5bd7bb01e514d1b43b2356e951efc41b0c18bd
“Thushara1020” <thusharabawantha2001@gmail.com>
2026-02-11 05:56:45 +0530

    integrate product submission with backend API

--- src/app/component/additems/additems.component.ts
+++ src/app/component/additems/additems.component.ts
@@ -1,21 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { CartService } from '../../../cart.service';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-additems',
-  standalone: true, // Standalone component nisa meka thiyenna oni
-  imports: [ReactiveFormsModule, CommonModule], // Methana aniwaaryen thiyenna oni
+  standalone: true,
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './additems.component.html',
   styleUrl: './additems.component.css'
 })
 export class AdditemsComponent implements OnInit {
   productForm!: FormGroup;
 
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder, private cartService: CartService , private router: Router) {}
 
   ngOnInit(): void {
-    // Form eka initialize kireema
     this.productForm = this.fb.group({
       name: ['', [Validators.required]],
       image: ['', [Validators.required]],
@@ -24,14 +26,28 @@ export class AdditemsComponent implements OnInit {
     });
   }
 
-  onSubmit() {
-    if (this.productForm.valid) {
-      console.log('Product Data:', this.productForm.value);
-  
-      alert('Product added successfully!');
-      this.productForm.reset({ status: 'available' }); 
-    } else {
-      alert('Please fill all required fields!');
-    }
+onSubmit() {
+  if (this.productForm.valid) {
+    const formData = this.productForm.value;
+
+    this.cartService.addItemToApi(formData).subscribe({
+      next: (response) => {
+        console.log('Saved successfully:', response);
+        alert('Product added successfully!');
+        
+    
+        this.productForm.reset({
+          status: 'available'
+        });
+        this.router.navigate(['/manage-items']);
+      },
+      error: (error) => {
+        console.error('Error saving product:', error);
+        alert('Failed to save product. Please try again.');
+      }
+    });
+  } else {
+    alert('Please fill all the required fields!');
   }
+}
 }
