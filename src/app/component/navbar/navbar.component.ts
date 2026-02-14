cd601743916a3e5ce6bfcb132b592bc8de5c1e28
“Thushara1020” <thusharabawantha2001@gmail.com>
2026-02-14 22:50:00 +0530

    implement dark mode logic in NavbarComponent

--- src/app/component/navbar/navbar.component.ts
+++ src/app/component/navbar/navbar.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core'; // OnInit එකතු කරන්න
 import { RouterLink, RouterLinkActive } from "@angular/router";
 
 @Component({
   selector: 'app-navbar',
+  standalone: true,
   imports: [RouterLink, RouterLinkActive],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit { 
+  
+  isDarkMode: boolean = false;
 
+  ngOnInit() {
+    const savedTheme = localStorage.getItem('theme');
+    
+    if (savedTheme === 'dark') {
+      this.isDarkMode = true;
+      document.body.classList.add('dark-mode');
+    }
+  }
+
+  toggleTheme(event: any) {
+    this.isDarkMode = event.target.checked;
+    
+    if (this.isDarkMode) {
+      document.body.classList.add('dark-mode');
+      localStorage.setItem('theme', 'dark'); 
+    } else {
+      document.body.classList.remove('dark-mode');
+      localStorage.setItem('theme', 'light');
+    }
+  }
 }
